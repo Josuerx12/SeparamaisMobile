ad0d39e89fea57d0b423d99856f9199515806bd9
josuerx12 <josueaze12@gmail.com>
2024-05-23 16:22:07 -0300

    feat: new features.

---
+++ src/routes/AuthorizedTabRoutes.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome, FontAwesome6 } from "@expo/vector-icons";
+import RequesterRoutes from "./requester/RequesterRoutes";
+import { useAuth } from "../contexts/AuthContext";
+import AlmoxScreen from "../screens/almox";
+import SettingsScreen from "../screens/settings";
+
+const AuthorizedTabRoutes = () => {
+  const Tabs = createBottomTabNavigator();
+  const { user } = useAuth();
+
+  return (
+    <Tabs.Navigator screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="requests"
+        component={RequesterRoutes}
+        options={{
+          headerTitleAlign: "center",
+          tabBarLabel: "Solicitações",
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome name="tags" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="billings"
+        component={AlmoxScreen}
+        options={{
+          tabBarLabel: "Compras",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <FontAwesome name="shopping-bag" size={size} color={color} />
+            );
+          },
+        }}
+      />
+      {user?.almox && (
+        <Tabs.Screen
+          name="almox"
+          component={AlmoxScreen}
+          options={{
+            tabBarLabel: "Alomoxarifado",
+            headerTitleAlign: "center",
+            tabBarIcon: ({ color, focused, size }) => {
+              return <FontAwesome name="archive" size={size} color={color} />;
+            },
+          }}
+        />
+      )}
+      {user?.admin && (
+        <Tabs.Screen
+          name="admin"
+          component={AlmoxScreen}
+          options={{
+            tabBarLabel: "Administração",
+            headerTitleAlign: "center",
+            tabBarIcon: ({ color, focused, size }) => {
+              return <FontAwesome name="shield" size={size} color={color} />;
+            },
+          }}
+        />
+      )}
+      <Tabs.Screen
+        name="settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: "Configurações",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome name="gear" size={size} color={color} />;
+          },
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
+
+export default AuthorizedTabRoutes;
