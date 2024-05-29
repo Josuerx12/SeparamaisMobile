c80f67509008ec670b97b821f174b2a639ef97de
josuerx12 <josueaze12@gmail.com>
2024-05-29 13:43:36 -0300

    feat: some improvements.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -43,26 +43,6 @@ export const AuthContextProvier = ({
 
   async function signIn(credentials: TSignInCredentials) {
     try {
-      if (Platform.OS === "android") {
-        Notifications.setNotificationChannelAsync("default", {
-          name: "default",
-          importance: Notifications.AndroidImportance.MAX,
-          vibrationPattern: [0, 250, 250, 250],
-          lightColor: "#FF231F7C",
-        });
-      }
-
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== "granted") {
-        return;
-      }
-
       const projectId =
         Constants?.expoConfig?.extra?.eas?.projectId ??
         Constants?.easConfig?.projectId;
@@ -128,8 +108,31 @@ export const AuthContextProvier = ({
     setLoading(false);
   };
 
+  const getNotificationsAuth = async () => {
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      return;
+    }
+  };
+
   useEffect(() => {
     loadTokenFromAsyncStorage();
+    getNotificationsAuth();
   }, []);
 
   return (
